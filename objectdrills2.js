2e72b76c93c3e77ef25a6d2e50b900c3fb9deb35
JT Liner <[jtliner925@gmail.com]>
2019-12-03 17:13:17 -0600

    makeStudentReport function

--- objectdrills2.js
+++ objectdrills2.js
@@ -1,17 +1,45 @@
 'use strict';
 
 const data = [
-    { name: 'Jane Doe', grade: 'A' },
-    { name: 'John Dough', grade: 'B' },
-    { name: 'Jill Do', grade: 'A' },
-  ];
+  { name: 'Jane Doe', grade: 'A' },
+  { name: 'John Dough', grade: 'B' },
+  { name: 'Jill Do', grade: 'A' },
+];
 
 function makeStudentsReport(data) {
-    return data.map(function(item) {
+  return data.map(function(item) {
 
-        return `${item.name} : ${item.grade}`;
+    return `${item.name} : ${item.grade}`;
         
-    });
+  });
 }
 
-console.log(makeStudentsReport(data));
+//console.log(makeStudentsReport(data));
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+  
+function enrollInSummerSchool(students) {
+  return students.map(student => {
+    return {
+      name: student.name,
+      status: 'In Summer school',
+      course: student.course,
+    };
+  });
+}
+console.log(enrollInSummerSchool(studentData));
